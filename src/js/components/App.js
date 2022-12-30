e50e339a45602dc2191284764a58e6536ac55407
Jake Zatecky <jake.zatecky@gmail.com>
2022-12-30 05:40:09 -0500

    Split settings into its own component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
 
-import AutocompleteControl from 'js/components/Controls/AutocompleteControl';
-import SliderControl from 'js/components/Controls/SliderControl';
 import DraftResults from 'js/components/DraftResults';
+import DraftSettings from 'js/components/DraftSettings';
 import LoadingIndicator from 'js/components/Utils/LoadingIndicator';
-import getLanguage from 'js/utils/getLanguage';
-import draftLeaders from 'js/draftLeaders';
 
 class App extends Component {
     constructor(props) {
@@ -15,14 +11,9 @@ class App extends Component {
 
         this.state = {
             leaders: null,
-            numPlayers: 6,
-            numChoices: 3,
-            bans: [],
             results: null,
         };
 
-        this.onSliderChange = this.onSliderChange.bind(this);
-        this.onBanChange = this.onBanChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
@@ -44,118 +35,8 @@ class App extends Component {
         });
     }
 
-    onSliderChange(key) {
-        return (event) => {
-            this.setState({ [key]: event.target.value });
-        };
-    }
-
-    onBanChange(event, value) {
-        this.setState({ bans: value });
-    }
-
-    onSubmit(event) {
-        event.preventDefault();
-
-        const {
-            leaders,
-            numPlayers,
-            numChoices,
-            bans,
-        } = this.state;
-
-        const totalChoices = numPlayers * numChoices;
-        const availableLeaders = leaders.length - bans.length;
-        const notEnoughLeaders = totalChoices > availableLeaders;
-
-        if (notEnoughLeaders) {
-            this.setState({
-                results: {
-                    error: getLanguage('notEnoughLeaders', {
-                        totalChoices,
-                        availableLeaders,
-                    }),
-                },
-            });
-            return;
-        }
-
-        // TODO: Verify that multiple players can play different personas
-        const draftResults = draftLeaders(leaders, numPlayers, numChoices, bans);
-
-        this.setState({
-            results: {
-                players: draftResults,
-            },
-        });
-    }
-
-    renderMainSettings() {
-        const { numPlayers, numChoices } = this.state;
-
-        return (
-            <Accordion.Item eventKey="0">
-                <Accordion.Header as="h3">Main settings</Accordion.Header>
-                <Accordion.Body>
-                    <SliderControl
-                        id="num-players"
-                        label="Number of players"
-                        max={12}
-                        min={1}
-                        value={numPlayers}
-                        onChange={this.onSliderChange('numPlayers')}
-                    />
-                    <SliderControl
-                        id="num-choices"
-                        label="Number of choices"
-                        max={6}
-                        min={1}
-                        value={numChoices}
-                        onChange={this.onSliderChange('numChoices')}
-                    />
-                </Accordion.Body>
-            </Accordion.Item>
-        );
-    }
-
-    renderAdditionalSettings() {
-        const { bans, leaders } = this.state;
-        const leaderOptions = leaders.map(({ name, image }) => ({
-            value: name,
-            name,
-            label: (
-                <div className="leader-box">
-                    <img alt={`${name} portrait}`} className="leader-icon" src={`/assets/img/leader-icons/${image}`} />
-                    <span className="leader-name">{`${name}`}</span>
-                </div>
-            ),
-        }));
-
-        return (
-            <Accordion.Item eventKey="1">
-                <Accordion.Header as="h3">Additional settings</Accordion.Header>
-                <Accordion.Body>
-                    <AutocompleteControl
-                        label="Banned leaders"
-                        options={leaderOptions}
-                        value={bans}
-                        onChange={this.onBanChange}
-                    />
-                </Accordion.Body>
-            </Accordion.Item>
-        );
-    }
-
-    renderUserSettings() {
-        // TODO: Implement
-        return (
-            <Accordion.Item eventKey="2">
-                <Accordion.Header as="h3">User-specific settings</Accordion.Header>
-                <Accordion.Body>
-                    List all users and have DLC checks
-                </Accordion.Body>
-            </Accordion.Item>
-        );
+    onSubmit(results) {
+        this.setState({ results });
     }
 
     render() {
@@ -168,19 +49,7 @@ class App extends Component {
         return (
             <section className="draft-area">
                 <h1 className="sr-only">Draft picker</h1>
-                <h2 className="sr-only">Draft selections</h2>
-                <form className="form" onSubmit={this.onSubmit}>
-                    <Accordion alwaysOpen defaultActiveKey={['0', '1']}>
-                        {this.renderMainSettings()}
-                        {this.renderAdditionalSettings()}
-                        {this.renderUserSettings()}
-                    </Accordion>
-                    <div className="form-submit-area">
-                        <button className="btn btn-primary" type="submit">
-                            Draft!
-                        </button>
-                    </div>
-                </form>
+                <DraftSettings leaders={leaders} onSubmit={this.onSubmit} />
                 {results !== null ? <DraftResults results={results} /> : null}
             </section>
         );
---
+++ src/js/components/DraftSettings.js
@@ -0,0 +1,156 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import SliderControl from './Controls/SliderControl';
+import AutocompleteControl from './Controls/AutocompleteControl';
+
+import leaderShape from 'js/shapes/leaderShape';
+import getLanguage from 'js/utils/getLanguage';
+import draftLeaders from 'js/draftLeaders';
+
+class DraftSettings extends Component {
+    static propTypes = {
+        leaders: PropTypes.arrayOf(leaderShape).isRequired,
+        onSubmit: PropTypes.func.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numPlayers: 6,
+            numChoices: 3,
+            bans: [],
+        };
+
+        this.onSliderChange = this.onSliderChange.bind(this);
+        this.onBanChange = this.onBanChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSliderChange(key) {
+        return (event) => {
+            this.setState({ [key]: event.target.value });
+        };
+    }
+
+    onBanChange(event, value) {
+        this.setState({ bans: value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        const { leaders, onSubmit } = this.props;
+        const { numPlayers, numChoices, bans } = this.state;
+
+        const totalChoices = numPlayers * numChoices;
+        const availableLeaders = leaders.length - bans.length;
+        const notEnoughLeaders = totalChoices > availableLeaders;
+
+        if (notEnoughLeaders) {
+            onSubmit({
+                error: getLanguage('notEnoughLeaders', {
+                    totalChoices,
+                    availableLeaders,
+                }),
+            });
+        } else {
+            // TODO: Verify that multiple players can play different personas
+            const draftResults = draftLeaders(leaders, numPlayers, numChoices, bans);
+
+            onSubmit({ players: draftResults });
+        }
+    }
+
+    renderMainSettings() {
+        const { numPlayers, numChoices } = this.state;
+
+        return (
+            <Accordion.Item eventKey="0">
+                <Accordion.Header as="h3">Main settings</Accordion.Header>
+                <Accordion.Body>
+                    <SliderControl
+                        id="num-players"
+                        label="Number of players"
+                        max={12}
+                        min={1}
+                        value={numPlayers}
+                        onChange={this.onSliderChange('numPlayers')}
+                    />
+                    <SliderControl
+                        id="num-choices"
+                        label="Number of choices"
+                        max={6}
+                        min={1}
+                        value={numChoices}
+                        onChange={this.onSliderChange('numChoices')}
+                    />
+                </Accordion.Body>
+            </Accordion.Item>
+        );
+    }
+
+    renderAdditionalSettings() {
+        const { leaders } = this.props;
+        const { bans } = this.state;
+        const leaderOptions = leaders.map(({ name, image }) => ({
+            value: name,
+            name,
+            label: (
+                <div className="leader-box">
+                    <img alt={`${name} portrait}`} className="leader-icon" src={`/assets/img/leader-icons/${image}`} />
+                    <span className="leader-name">{`${name}`}</span>
+                </div>
+            ),
+        }));
+
+        return (
+            <Accordion.Item eventKey="1">
+                <Accordion.Header as="h3">Additional settings</Accordion.Header>
+                <Accordion.Body>
+                    <AutocompleteControl
+                        label="Banned leaders"
+                        options={leaderOptions}
+                        value={bans}
+                        onChange={this.onBanChange}
+                    />
+                </Accordion.Body>
+            </Accordion.Item>
+        );
+    }
+
+    renderUserSettings() {
+        // TODO: Implement
+        return (
+            <Accordion.Item eventKey="2">
+                <Accordion.Header as="h3">User-specific settings</Accordion.Header>
+                <Accordion.Body>
+                    List all users and have DLC checks
+                </Accordion.Body>
+            </Accordion.Item>
+        );
+    }
+
+    render() {
+        return (
+            <div className="draft-settings">
+                <h2 className="sr-only">Draft selections</h2>
+                <form className="form" onSubmit={this.onSubmit}>
+                    <Accordion alwaysOpen defaultActiveKey={['0', '1']}>
+                        {this.renderMainSettings()}
+                        {this.renderAdditionalSettings()}
+                        {this.renderUserSettings()}
+                    </Accordion>
+                    <div className="form-submit-area">
+                        <button className="btn btn-primary" type="submit">
+                            Draft!
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default DraftSettings;
