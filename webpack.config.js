f03bac199b0fd09ae162ca9698aa668bc44f44df
Jake Zatecky <jake.zatecky@gmail.com>
2023-01-24 00:37:05 -0500

    Simplify build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('node:fs');
 const path = require('node:path');
 
-function getEnvironmentParameters(stage) {
-    let env = {};
+const environment = process.env.NODE_ENV;
+const isProduction = environment === 'production';
 
-    try {
-        const data = fs.readFileSync(`public/env.${stage}.json`, 'utf8');
-        env = JSON.parse(data);
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            // eslint-disable-next-line no-console
-            console.log(`Environment file not found for stage '${stage}'. Skipping.`);
-        } else {
-            throw error;
-        }
-    }
-
-    return `<script>window.env = ${JSON.stringify(env)};</script>`;
-}
-
-module.exports = ({ stage = 'local' }) => ({
-    mode: stage === 'prod' ? 'production' : 'development',
+module.exports = () => ({
+    mode: isProduction ? 'production' : 'development',
     entry: {
         app: './src/js/entry.js',
     },
     output: {
         path: path.join(__dirname, '/public'),
-        filename: stage === 'prod' ? 'assets/js/[name]-[contenthash].js' : 'assets/js/[name].js',
+        filename: isProduction ? 'assets/js/[name]-[contenthash].js' : 'assets/js/[name].js',
         publicPath: '/',
     },
     resolve: {
@@ -59,20 +43,17 @@ module.exports = ({ stage = 'local' }) => ({
         watchFiles: ['src/**/*'],
     },
     performance: {
-        hints: stage === 'prod' ? 'warning' : false,
+        hints: isProduction ? 'warning' : false,
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: stage === 'prod' ? 'assets/css/[name]-[contenthash].css' : 'assets/css/[name].css',
+            filename: isProduction ? 'assets/css/[name]-[contenthash].css' : 'assets/css/[name].css',
         }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             filename: 'index.html',
             inject: false,
             minify: false,
-            templateParameters: {
-                env: getEnvironmentParameters(stage),
-            },
         }),
     ],
 });
