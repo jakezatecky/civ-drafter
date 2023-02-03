bbf5c829fce46fc43e0224782fdc72532f90a839
Jake Zatecky <jake.zatecky@gmail.com>
2023-02-03 14:34:21 -0500

    Fix typo

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -15,7 +15,7 @@ async function generateSitemap() {
         urlset: {
             '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
             url: [{
-                loc: 'https://civilizationdrater.com',
+                loc: 'https://civilizationdrafter.com',
                 changefreq: 'weekly',
                 priority: '1.0',
                 lastmod: (new Date()).toISOString().split('T')[0],
