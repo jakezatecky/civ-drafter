1b2ae0ae792c3a439b9a88288b5b7e88f47cc3a0
Jake Zatecky <jake.zatecky@gmail.com>
2023-02-28 21:14:17 -0500

    Make `DraftResults` functional

--- src/js/components/DraftResults.jsx
+++ src/js/components/DraftResults.jsx
@@ -1,86 +1,84 @@
 import random from 'lodash/random';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import Alert from 'js/components/Utils/Alert';
 import { trollFactor } from 'js/constants/calculation';
 import leaderShape from 'js/shapes/leaderShape';
 
-class DraftResults extends PureComponent {
-    static propTypes = {
-        results: PropTypes.shape({
-            players: PropTypes.arrayOf(PropTypes.shape({
-                index: PropTypes.number.isRequired,
-                choices: PropTypes.arrayOf(leaderShape).isRequired,
-            })),
-            error: PropTypes.string,
-        }).isRequired,
-    };
+const propTypes = {
+    results: PropTypes.shape({
+        players: PropTypes.arrayOf(PropTypes.shape({
+            index: PropTypes.number.isRequired,
+            choices: PropTypes.arrayOf(leaderShape).isRequired,
+        })),
+        error: PropTypes.string,
+    }).isRequired,
+};
 
-    getPlayerChoices(players) {
-        // Must be more than one player and we must roll a zero
-        // Only then can Capn Crunch bless us
-        const timeToTroll = players.length > 1 && random(0, trollFactor) === 0;
+function getPlayerChoices(players) {
+    // Must be more than one player and we must roll a zero
+    // Only then can Cap'n Crunch bless us
+    const timeToTroll = players.length > 1 && random(0, trollFactor) === 0;
 
-        if (!timeToTroll) {
-            return { trollLeader: null, players };
-        }
-
-        // Everybody's choices become the first player's first choice
-        const numChoices = players[0].choices.length;
-        const trollLeader = players[0].choices[0];
-        const newPlayerChoices = players.map(({ index }) => ({
-            index,
-            choices: Array(numChoices).fill(trollLeader),
-        }));
-
-        return { trollLeader, players: newPlayerChoices };
+    if (!timeToTroll) {
+        return { trollLeader: null, players };
     }
 
-    render() {
-        const { results } = this.props;
-        const { error, players } = results;
+    // Everybody's choices become the first player's first choice
+    const numChoices = players[0].choices.length;
+    const trollLeader = players[0].choices[0];
+    const newPlayerChoices = players.map(({ index }) => ({
+        index,
+        choices: Array(numChoices).fill(trollLeader),
+    }));
 
-        if (error) {
-            return <Alert type="danger">{error}</Alert>;
-        }
-
-        const { trollLeader, players: actualPlayers } = this.getPlayerChoices(players);
-
-        /* eslint-disable react/no-array-index-key */
-        const formatted = actualPlayers.map(({ index: playerIndex, choices }) => (
-            <li key={playerIndex}>
-                <h3>{`Player ${playerIndex + 1}`}</h3>
-                <ul className="draft-results-player-choices">
-                    {choices.map(({ shortName, image }, choiceIndex) => (
-                        <li key={`${playerIndex}-${choiceIndex}-${shortName}`}>
-                            <img alt={`${shortName} icon`} src={`/assets/img/leader-icons/${image}`} />
-                            {shortName}
-                        </li>
-                    ))}
-                </ul>
-            </li>
-        ));
-        /* eslint-enable react/no-array-index-key */
-
-        return (
-            <div className="draft-results">
-                <h2 className="visually-hidden">Draft results</h2>
-                {trollLeader !== null ? (
-                    <Alert
-                        language={{
-                            key: 'oopsAllSameLeader',
-                            variables: { leaderName: trollLeader.shortName },
-                        }}
-                        type="warning"
-                    />
-                ) : null}
-                <ul className="draft-results-players">
-                    {formatted}
-                </ul>
-            </div>
-        );
-    }
+    return { trollLeader, players: newPlayerChoices };
 }
+
+/* eslint-disable react/no-array-index-key */
+function DraftResults({ results }) {
+    const { error, players } = results;
+
+    if (error) {
+        return <Alert type="danger">{error}</Alert>;
+    }
+
+    const { trollLeader, players: actualPlayers } = getPlayerChoices(players);
+
+    const formatted = actualPlayers.map(({ index: playerIndex, choices }) => (
+        <li key={playerIndex}>
+            <h3>{`Player ${playerIndex + 1}`}</h3>
+            <ul className="draft-results-player-choices">
+                {choices.map(({ shortName, image }, choiceIndex) => (
+                    <li key={`${playerIndex}-${choiceIndex}-${shortName}`}>
+                        <img alt={`${shortName} icon`} src={`/assets/img/leader-icons/${image}`} />
+                        {shortName}
+                    </li>
+                ))}
+            </ul>
+        </li>
+    ));
+
+    return (
+        <div className="draft-results">
+            <h2 className="visually-hidden">Draft results</h2>
+            {trollLeader !== null ? (
+                <Alert
+                    language={{
+                        key: 'oopsAllSameLeader',
+                        variables: { leaderName: trollLeader.shortName },
+                    }}
+                    type="warning"
+                />
+            ) : null}
+            <ul className="draft-results-players">
+                {formatted}
+            </ul>
+        </div>
+    );
+}
+
+DraftResults.propTypes = propTypes;
 
 export default DraftResults;
