029cbb1bb37bd5d562523f70263c8f55fd65bde7
Jake Zatecky <jake.zatecky@gmail.com>
2023-03-03 05:09:27 -0500

    Simplify ThemeSwitcher and make it a function
    Also adjusts dark mode header to be whiter.

--- src/js/components/ThemeSwitcher.jsx
+++ src/js/components/ThemeSwitcher.jsx
@@ -1,69 +1,70 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { FormControlLabel, Switch } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
+import React, {
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
 
-import materialTheme from 'js/components/materialTheme';
+const propTypes = {
+    initialTheme: PropTypes.string.isRequired,
+};
 
-class ThemeSwitcher extends Component {
-    static propTypes = {
-        initialTheme: PropTypes.string.isRequired,
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            theme: props.initialTheme,
-        };
-
-        this.onChange = this.onChange.bind(this);
-    }
-
-    componentDidMount() {
-        const { theme } = this.state;
-
-        this.setRootTheme(theme);
-    }
-
-    onChange(event) {
-        const darkMode = event.target.checked;
-        const theme = darkMode ? 'dark' : 'light';
-
-        this.setState({ theme }, () => {
-            this.setRootTheme(theme);
-            this.saveTheme(theme);
-        });
-    }
-
-    setRootTheme(theme) {
-        document.querySelector('html').dataset.bsTheme = theme;
-    }
-
-    saveTheme(theme) {
-        localStorage.setItem('theme', theme);
-    }
-
-    render() {
-        const { theme } = this.state;
-        const darkMode = theme === 'dark';
-        const label = (
-            <span className="theme-label">
-                <span className="fa fa-circle-half-stroke" />
-            </span>
-        );
-
-        return (
-            <ThemeProvider theme={materialTheme(theme)}>
-                <FormControlLabel
-                    aria-label="Dark mode"
-                    control={<Switch checked={darkMode} color="default" onChange={this.onChange} />}
-                    label={label}
-                    title="Toggle theme"
-                />
-            </ThemeProvider>
-        );
-    }
+function setRootTheme(theme) {
+    document.querySelector('html').dataset.bsTheme = theme;
 }
+
+function saveTheme(theme) {
+    localStorage.setItem('theme', theme);
+}
+
+function ThemeSwitcher({ initialTheme }) {
+    const [theme, setTheme] = useState(initialTheme);
+    const isFirstRender = useRef(true);
+    const isDarkMode = theme === 'dark';
+
+    // Set initial root theme
+    useEffect(() => {
+        setRootTheme(theme);
+    }, []);
+
+    // Save user's changes
+    useEffect(() => {
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+
+        setRootTheme(theme);
+        saveTheme(theme);
+    }, [theme]);
+
+    // Toggle the user's theme
+    const onChange = useCallback(() => {
+        setTheme(isDarkMode ? 'light' : 'dark');
+    }, [theme]);
+
+    const label = isDarkMode ? 'Switch to light theme' : 'Switch to dark theme';
+    const className = classNames({
+        'theme-switcher': true,
+        'theme-dark': isDarkMode,
+        'theme-light': !isDarkMode,
+    });
+
+    return (
+        <button
+            aria-label={label}
+            className={className}
+            title={label}
+            type="button"
+            onClick={onChange}
+        >
+            <span className="fa fa-circle-half-stroke" />
+        </button>
+    );
+}
+
+ThemeSwitcher.propTypes = propTypes;
 
 export default ThemeSwitcher;
