369cd4ffdad056db82babc1c105804d54308821a
Jake Zatecky <jake.zatecky@gmail.com>
2023-03-06 02:14:12 -0500

    Convert App component to function

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,55 +1,37 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import DraftArea from 'js/components/DraftArea';
 import ThemeContext from 'js/contexts/ThemeContext';
 
-class App extends Component {
-    static propTypes = {
-        initialTheme: PropTypes.string.isRequired,
-    };
+const propTypes = {
+    initialTheme: PropTypes.string.isRequired,
+};
 
-    constructor(props) {
-        super(props);
+function App({ initialTheme }) {
+    const [theme, setTheme] = useState(initialTheme);
 
-        this.state = { theme: props.initialTheme };
-        this.onThemeChange = this.onThemeChange.bind(this);
-    }
-
-    componentDidMount() {
-        this.watchThemeChange();
-    }
-
-    onThemeChange(theme) {
-        this.setState({ theme });
-    }
-
-    /**
-     * Observe the HTML root for any theme changes.
-     *
-     * @returns {void}
-     */
-    watchThemeChange() {
+    // Observe the HTML root for any theme changes.
+    useEffect(() => {
         const htmlRoot = document.querySelector('html');
         const observer = new MutationObserver((mutationList) => {
             mutationList.forEach((mutation) => {
                 if (mutation.type === 'attributes') {
-                    this.onThemeChange(htmlRoot.dataset.bsTheme);
+                    setTheme(htmlRoot.dataset.bsTheme);
                 }
             });
         });
+
         observer.observe(htmlRoot, { attributes: true });
-    }
+    }, []);
 
-    render() {
-        const { theme } = this.state;
-
-        return (
-            <ThemeContext.Provider value={theme}>
-                <DraftArea />
-            </ThemeContext.Provider>
-        );
-    }
+    return (
+        <ThemeContext.Provider value={theme}>
+            <DraftArea />
+        </ThemeContext.Provider>
+    );
 }
+
+App.propTypes = propTypes;
 
 export default App;
