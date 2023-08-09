635f52e43a61a0955476dc3df8e911b53d47ce66
Jake Zatecky <jake.zatecky@gmail.com>
2023-08-09 04:20:56 -0400

    Fix import order

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 const path = require('node:path');
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 const webpack = require('webpack');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
